1e182ac7360b2851d0d18bbc349dc122ffeea65e
Brett Watkins <brettderek13147@gmail.com>
2025-08-27 22:02:17 -0500

    Added support for ImGui. Created a test window. ImGui dependencies are in /imgui. /Application.cpp is the main file with Task Manager specific code.

---
+++ TaskGUI/Application.cpp
@@ -0,0 +1,14 @@
+#include "Application.h"
+#include "imgui/imgui.h"
+
+namespace Tasking{
+
+	void RenderUI(){
+	
+	ImGui::Begin("Tasks");
+	ImGui::Button("Halp");
+	ImGui::End();
+
+	}
+
+}
---
+++ TaskGUI/Application.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Tasking{
+
+	void RenderUI();
+
+}
