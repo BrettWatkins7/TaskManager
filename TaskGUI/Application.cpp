d6fe88cf2138949515983dfe8b6a473fbc2f7c9f
Brett Watkins <brettderek13147@gmail.com>
2025-08-29 16:24:20 -0500

    Finished initial build of the Task Manager GUI app using Dear ImGui

--- TaskGUI/Application.cpp
+++ TaskGUI/Application.cpp
@@ -1,21 +1,36 @@
 #include "Application.h"
 #include "imgui/imgui.h"
+#include "imgui/imgui_stdlib.h"
 #include <string>
+#include <vector>
 
-struct Task{
+struct task{
 	std::string taskName;
-	bool taskStatus;
+	bool taskStatus = false;
 };
 
-Task myTask{"Testing1", false};
+std::vector<task> tasks;
+std::string placeholder = "";
 
 namespace Tasking{
 
 	void RenderUI(){
 
 	ImGui::Begin("Tasks");
-	ImGui::Button("Halp");
-	ImGui::Text("%s", myTask.taskName.c_str());
+	ImGui::SetNextItemWidth(300);
+	ImGui::InputText(" ", &placeholder);
+	ImGui::SameLine();
+	if(ImGui::Button("Add Task")){
+		tasks.push_back({placeholder});
+		placeholder.clear();
+	}
+	for(size_t i = 0; i < tasks.size(); i++){
+		ImGui::Text("%s", tasks[i].taskName.c_str());
+		ImGui::SameLine();
+		if(ImGui::Button(("Remove##" + std::to_string(i)).c_str())){
+			tasks.erase(tasks.begin() + i);
+		}
+	}
 	ImGui::End();
 
 	}
