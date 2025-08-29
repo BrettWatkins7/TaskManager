bb6bdfb9bfcff7660f9406ed359a49da7e12da92
Brett Watkins <brettderek13147@gmail.com>
2025-08-28 21:40:34 -0500

    Removed unused variables and tested text output

--- TaskGUI/Application.cpp
+++ TaskGUI/Application.cpp
@@ -1,12 +1,21 @@
 #include "Application.h"
 #include "imgui/imgui.h"
+#include <string>
+
+struct Task{
+	std::string taskName;
+	bool taskStatus;
+};
+
+Task myTask{"Testing1", false};
 
 namespace Tasking{
 
 	void RenderUI(){
-	
+
 	ImGui::Begin("Tasks");
 	ImGui::Button("Halp");
+	ImGui::Text("%s", myTask.taskName.c_str());
 	ImGui::End();
 
 	}
