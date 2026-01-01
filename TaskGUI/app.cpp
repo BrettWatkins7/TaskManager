74720ddc85852d0ca2f22434657508938745b9ea
Brett Watkins <brettderek13147@gmail.com>
2025-12-31 22:37:46 -0600

    Added ability to submit tasks using 'Enter' key

--- TaskGUI/app.cpp
+++ TaskGUI/app.cpp
@@ -10,30 +10,39 @@ struct task{
 };
 
 std::vector<task> tasks;
-std::string placeholder = "";
+std::string textInput = "";
+ImGuiInputTextFlags flags = ImGuiInputTextFlags_EnterReturnsTrue;
+
+void addTask(){
+	tasks.push_back({textInput});
+	textInput.clear();
+}
 
 namespace Tasking{
 
 	void RenderUI(){
-	ImGui::SetNextWindowPos(ImVec2(0, 0));
-	ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
-	ImGui::Begin("Tasks", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
-	ImGui::SetNextItemWidth(300);
-	ImGui::InputText(" ", &placeholder);
-	ImGui::SameLine();
-	if(ImGui::Button("Add Task")){
-		tasks.push_back({placeholder});
-		placeholder.clear();
-	}
-	for(size_t i = 0; i < tasks.size(); i++){
-		ImGui::Text("%s", tasks[i].taskName.c_str());
-		ImGui::SameLine();
-		if(ImGui::Button(("Remove##" + std::to_string(i)).c_str())){
-			tasks.erase(tasks.begin() + i);
+		ImGui::SetNextWindowPos(ImVec2(0, 0));
+		ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+		ImGui::Begin("Tasks", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+		ImGui::SetNextItemWidth(300);
+		if(ImGui::InputTextWithHint("##Task Name", "Task Name...", &textInput, flags) && textInput.length() != 0){
+			tasks.push_back({textInput});
+			textInput.clear();
 		}
+		ImGui::SameLine();
+		if(ImGui::Button("Add Task")){
+			if(textInput.length() != 0){
+				tasks.push_back({textInput});
+				textInput.clear();
+			}
+		}
+		for(size_t i = 0; i < tasks.size(); i++){
+			ImGui::Text("%s", tasks[i].taskName.c_str());
+			ImGui::SameLine();
+			if(ImGui::Button(("Remove##" + std::to_string(i)).c_str())){
+				tasks.erase(tasks.begin() + i);
+			}
+		}
+		ImGui::End();
 	}
-	ImGui::End();
-
-	}
-
 }
